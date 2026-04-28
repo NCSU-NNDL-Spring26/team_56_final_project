0d0321512013d665c57104c6368836cc8783856c
Charlie Duchene <cdduchene4@gmail.com>
2026-04-28 13:06:08 -0400

    Editing For Readability

--- Neural_Network/evaluate.py
+++ Neural_Network/evaluate.py
@@ -1,1 +1,6 @@
+from sklearn.metrics import classification_report, confusion_matrix
 
+
+def evaluate_predictions(y_true, y_pred, class_names):
+    print(classification_report(y_true, y_pred, target_names=class_names))
+    print(confusion_matrix(y_true, y_pred))
